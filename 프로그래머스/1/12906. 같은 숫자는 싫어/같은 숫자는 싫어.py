57ef8b89bebef62da1ea79a85ebbdb6a42937a92
strdeok <ejraks1548@gmail.com>
2025-09-15 19:59:48 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 147.55 ms, Memory: 27.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = []
+    for i in arr:
+        if len(answer) == 0:
+            answer.append(i)
+        elif answer[-1] != i:
+            answer.append(i)
+    return answer
