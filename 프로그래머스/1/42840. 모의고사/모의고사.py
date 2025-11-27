59f32a11d7ce3bfc968bb11f00dac5ce7f548f60
strdeok <ejraks1548@gmail.com>
2025-11-27 16:57:04 +0900

    [level 1] Title: 모의고사, Time: 3.06 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,20 @@
 def solution(answers):
-    answer = [0, 0, 0]
-    one_pattern = [1, 2, 3, 4, 5]
-    two_pattern = [2,1,2,3,2,4,2,5]
-    third_pattern = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] 
-
+    answer = []
+    p_1 = [1, 2, 3, 4, 5]
+    p_2 = [2,1,2,3,2,4,2,5]
+    p_3 = [3,3,1,1,2,2,4,4,5,5]
+    arr = [0,0,0]
     
     for i in range(len(answers)):
-        if answers[i] == one_pattern[i%5]:
-            answer[0] += 1
-        if answers[i] == two_pattern[i%8]:
-            answer[1] += 1
-        if answers[i] == third_pattern[i%10]:
-            answer[2] += 1
+        if p_1[i%5] == answers[i]:
+            arr[0] += 1
+        if p_2[i%len(p_2)] == answers[i]:
+            arr[1] += 1
+        if p_3[i%len(p_3)] == answers[i]:
+            arr[2] += 1
             
-    a = []
-    max_score = -1
-    for j, k in enumerate(answer):
-        if k > max_score:
-            a = [j+1]
-            max_score = k
-        elif k == max_score:
-            a.append(j+1)
-    return a
+    high = max(arr)
+    for i in range(len(arr)):
+        if arr[i] == high:
+            answer.append(i+1)
+    return answer
