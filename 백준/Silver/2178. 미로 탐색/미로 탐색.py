ac0a2731c39fd858529c6341d301a2446970ff80
strdeok <ejraks1548@gmail.com>
2025-11-29 18:54:21 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+n, m = map(int, input().split())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
+    q = deque()
+    q.append((x,y))
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            elif graph[nx][ny] == 0:
+                continue
+            elif graph[nx][ny] == 1:
+                q.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+    return graph[n-1][m-1]
+print(bfs(0,0))
