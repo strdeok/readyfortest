bc4e0a8f989ac9eb8623654e18cd1ca76db17bcc
strdeok <ejraks1548@gmail.com>
2026-02-06 10:24:47 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 105 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 
 class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        for (int test_case = 1; test_case <= 10; test_case++) {
-        	int T = sc.nextInt();
-        	int n = sc.nextInt();
-        	int m = sc.nextInt();
-        	int[] arr = new int[m];
-        	
-        	for (int i = 0; i < m; i++) {
-        		arr[i] = n;
-        	}
-        	
-        	int result = 1;
-        	for (int i = m - 1; i >= 0; i--) {
-        		result *= arr[i];
-        	}
-        	
-            
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			int T = sc.nextInt();
+			int n = sc.nextInt();
+			int m = sc.nextInt();
 
-            System.out.println("#" + T + " " + result);
-        }
-    }
+			System.out.println("#" + T + " " + jegop(n, m));
+		}
+	}
+
+	static int jegop(int n, int m) {
+		if (m == 1) {
+			return n;
+		} else {
+			return n * jegop(n, m - 1);
+		}
+
+	}
 }
