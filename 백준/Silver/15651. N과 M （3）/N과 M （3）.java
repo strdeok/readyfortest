1e8e9a31c719101d328c55d101eb0260091b9c5c
strdeok <ejraks1548@gmail.com>
2026-02-19 14:35:39 +0900

    [Silver III] Title: N과 M (3), Time: 496 ms, Memory: 71632 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+	static int[] com;
+	static int[] nums;
+	static int n, m; // m개 고르기
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		com = new int[m];
+		nums = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			nums[i] = i + 1;
+		}
+		combination(0);
+		System.out.println(sb);
+	}
+
+	private static void combination(int sidx) {
+		if (sidx == m) {			
+			for (int i : com) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		} else {			
+			for (int i = 0; i < n; i++) {
+				com[sidx] = nums[i];
+				combination(sidx+1);
+			}
+		}
+		
+	}
+}
