4d5cd9e1643901ea83a8bd60e96ee588ca27c1e6
strdeok <ejraks1548@gmail.com>
2026-02-24 17:31:36 +0900

    [Silver III] Title: N과 M (2), Time: 192 ms, Memory: 18372 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,36 +1,39 @@
 import java.util.*;
 
 public class Main {
+	static int n, m;
 	static int[] com;
-	static int[] nums;
-	static int n, m; // m개 고르기
+	static int[] num;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		m = sc.nextInt();
+		num = new int[n];
 		com = new int[m];
-		nums = new int[n];
+
 		for (int i = 0; i < n; i++) {
-			nums[i] = i + 1;
+			num[i] = i + 1;
 		}
+
 		combination(0, 0);
+
 	}
 
-	private static void combination(int idx, int sidx) {
-		if (sidx == m) {
-			for (int i : com) {
-				System.out.print(i + " ");
+	public static void combination(int idx, int tidx) {
+		if (tidx == m) {
+			for (int n : com) {
+				System.out.print(n + " ");
 			}
 			System.out.println();
 			return;
-		} else if (idx == n) {
-			return;
-		} else {
-			com[sidx] = nums[idx];
-			combination(idx + 1, sidx + 1);
-			combination(idx + 1, sidx);
 		}
-		
+		if (idx == n)
+			return;
+
+		com[tidx] = num[idx];
+		combination(idx + 1, tidx + 1);
+		combination(idx + 1, tidx);
+
 	}
 }
