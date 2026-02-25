01e4213afb83013d02f2ed1a237e57a39f1020bd
strdeok <ejraks1548@gmail.com>
2026-02-25 10:52:19 +0900

    [Silver III] Title: N과 M (6), Time: 176 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,43 +1,41 @@
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int m;
-    static boolean[] visited;
-    static int[] nums;
-    static int[] com;
-    static StringBuilder sb;
+	static int n, m;
+	static int[] com;
+	static int[] num;
+	static StringBuilder sb;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        visited = new boolean[n];
-        nums = new int[n];
-        com = new int[m];
-        sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		n = sc.nextInt();
+		m = sc.nextInt();
+		num = new int[n];
+		com = new int[m];
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        Arrays.sort(nums);
-        combination(0, 0);
-        System.out.println(sb);
-    }
-
-
-    public static void combination(int idx, int sidx) {
-        if (sidx == m) {
-            for (int i : com) {
-                sb.append(i).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-        if (idx == n) return;
-        com[sidx] = nums[idx];
-        combination(idx + 1, sidx + 1);
-        combination(idx + 1, sidx);
-    }
+		for (int i = 0; i < n; i++) {
+			num[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(num);
+		combination(0, 0);
+		System.out.println(sb.toString());
+	}
+	
+	public static void combination(int idx, int tidx) {
+		
+		if (tidx >= m) {
+			for (int c:com) {
+				sb.append(c).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		if (idx >= n) return;
+			
+		com[tidx] = num[idx];
+		combination(idx + 1, tidx + 1);
+		combination(idx + 1, tidx);
+	}
 }
