1c13864c1074a758963d621f0c08e4c987ed4be6
strdeok <ejraks1548@gmail.com>
2026-02-25 10:54:03 +0900

    [Silver III] Title: N과 M (7), Time: 664 ms, Memory: 127416 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+	static int n, m;
+	static int[] com;
+	static int[] num;
+	static StringBuilder sb;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		n = sc.nextInt();
+		m = sc.nextInt();
+		num = new int[n];
+		com = new int[m];
+
+		for (int i = 0; i < n; i++) {
+			num[i] = sc.nextInt();
+		}
+
+		Arrays.sort(num);
+		combination(0, 0);
+		System.out.println(sb.toString());
+	}
+
+	public static void combination(int idx, int tidx) {
+
+		if (tidx >= m) {
+			for (int c : com) {
+				sb.append(c).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		if (idx >= n)
+			return;
+
+		for (int i = 0; i < n; i++) {
+			com[tidx] = num[i];
+			combination(i, tidx + 1);
+		}
+
+	}
+}
