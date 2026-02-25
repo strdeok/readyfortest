d379377335502a72cedf21ebdc912cfcab468215
strdeok <ejraks1548@gmail.com>
2026-02-25 09:08:46 +0900

    [Silver III] Title: N과 M (3), Time: 492 ms, Memory: 71764 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,37 +1,42 @@
 import java.util.*;
 
 public class Main {
+	static int n, m;
 	static int[] com;
-	static int[] nums;
-	static int n, m; // m개 고르기
-	static StringBuilder sb = new StringBuilder();
+	static int[] num;
+	static StringBuilder sb;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
 		n = sc.nextInt();
 		m = sc.nextInt();
+		num = new int[n];
 		com = new int[m];
-		nums = new int[n];
-		
+
 		for (int i = 0; i < n; i++) {
-			nums[i] = i + 1;
+			num[i] = i + 1;
 		}
-		combination(0);
-		System.out.println(sb);
+
+		combination(0, 0);
+		System.out.println(sb.toString());
 	}
 
-	private static void combination(int sidx) {
-		if (sidx == m) {			
+	public static void combination(int idx, int tidx) {
+		if (tidx == m) {
 			for (int i : com) {
 				sb.append(i).append(" ");
 			}
 			sb.append("\n");
 			return;
-		} else {			
-			for (int i = 0; i < n; i++) {
-				com[sidx] = nums[i];
-				combination(sidx+1);
-			}
 		}
-		
+		if (idx == n)
+			return;
+
+		for (int i = idx; i < n; i++) {
+			com[tidx] = num[i];
+			combination(idx, tidx + 1);
+		}
+
 	}
 }
