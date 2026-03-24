c87f571e58dc510ab65693eec76134db114044b9
strdeok <ejraks1548@gmail.com>
2026-03-24 09:48:37 +0900

    [level 3] Title: 입국심사, Time: 108.28 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+	public long solution(int n, int[] times) {
+		long answer = 0;
+		Arrays.sort(times);
+
+		long left = 1;
+		long right = (long) times[times.length - 1] * n;
+
+		while (left <= right) {
+			long mid = (left + right) / 2;
+			long sum = 0;
+
+			for (int i = 0; i < times.length; i++) {
+				sum += mid / times[i];
+			}
+
+			if (sum >= n) {
+				answer = mid;
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+
+		return answer;
+	}
+}
