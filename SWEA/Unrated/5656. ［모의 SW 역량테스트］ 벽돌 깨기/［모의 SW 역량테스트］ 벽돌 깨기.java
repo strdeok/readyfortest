20ede51ebfa097e1ce902f87a72b004af303ae61
strdeok <ejraks1548@gmail.com>
2026-04-20 20:48:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 271 ms, Memory: 99,788 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -1,142 +1,130 @@
 import java.util.*;
+import java.io.*;
 
-class Solution {
-	static int n, w, h;
-	static int[][] map;
-	static int[] com;
-	static int MINBRICKS;
+public class Solution {
+    static int N;
+    static int W;
+    static int H;
+    static int[][] map;
 
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			n = sc.nextInt();
-			w = sc.nextInt();
-			h = sc.nextInt();
-			map = new int[h][w];
-			com = new int[n];
-			MINBRICKS = Integer.MAX_VALUE;
+    static int MinCnt;
 
-			for (int i = 0; i < h; i++) {
-				for (int k = 0; k < w; k++) {
-					map[i][k] = sc.nextInt();
-				}
-			}
 
-			shoot(0);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-			System.out.println("#" + test_case + " " + MINBRICKS);
-		}
-	}
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
+            map = new int[H][W];
+            MinCnt = Integer.MAX_VALUE;
 
-	// 쏠 라인 선택
-	static void shoot(int cnt) {
-		if (cnt == n) {
-			int[][] tempMap = new int[h][w];
-			for (int i = 0; i < h; i++) {
-				for (int j = 0; j < w; j++) {
-					tempMap[i][j] = map[i][j];
-				}
-			}
+            for (int x = 0; x < H; x++) {
+                st = new StringTokenizer(br.readLine());
+                for (int y = 0; y < W; y++) {
+                    map[x][y] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-			for (int c : com) {
-				int topR = findTopBrick(tempMap, c);
-				if (topR != -1) {
-					explode(tempMap, topR, c);
-					gravity(tempMap);
-				}
-			}
 
-			countBricks(tempMap);
-			return;
-		}
+            shoot(0, map);
 
-		for (int i = 0; i < w; i++) {
-			com[cnt] = i;
-			shoot(cnt + 1);
-		}
-	}
 
-	// 상위 블록 찾기
-	static int findTopBrick(int[][] tempMap, int c) {
-		for (int r = 0; r < h; r++) {
+            System.out.println("#" + tc + " " + MinCnt);
+        }
+    }
 
-			if (tempMap[r][c] != 0) {
-				return r;
-			}
-		}
-		return -1;
-	}
+    static void shoot(int cnt, int[][] currentMap) {
+        if (cnt == N) {
+            updateMax(currentMap);
+            return;
+        }
 
-	// 연쇄작용
-	// 좌표와 폭발 범위를 담을 객체 또는 배열이 필요합니다.
-	static void explode(int[][] tempMap, int r, int c) {
-		Queue<int[]> q = new LinkedList<>();
+        for (int y = 0; y < W; y++) {
+            int[][] backup = copyMap(currentMap);
 
-		if (tempMap[r][c] > 1) {
-			q.offer(new int[] { r, c, tempMap[r][c] });
-		}
-		tempMap[r][c] = 0;
+            int x = findTopBrick(y, backup);
 
-		int[] dx = { -1, 1, 0, 0 };
-		int[] dy = { 0, 0, -1, 1 };
+            if (x != -1) {
+                boom(x, y, backup);
+                drop(backup);
+            }
 
-		while (!q.isEmpty()) {
-			int[] now = q.poll();
-			int nowX = now[0];
-			int nowY = now[1];
-			int range = now[2];
+            shoot(cnt + 1, backup);
+        }
+    }
 
-			for (int i = 0; i < 4; i++) {
-				for (int k = 1; k < range; k++) {
-					int nx = nowX + (dx[i] * k);
-					int ny = nowY + (dy[i] * k);
+    static void updateMax(int[][] map) {
+        int cnt = 0;
+        for (int x = 0; x < H; x++) {
+            for (int y = 0; y < W; y++) {
+                if (map[x][y] > 0) {
+                    cnt++;
+                }
+            }
+        }
+        MinCnt = Math.min(cnt, MinCnt);
 
-					if (nx < 0 || nx >= h || ny < 0 || ny >= w)
-						continue;
+    }
 
-					// 4. 벽돌이 있다면?
-					if (tempMap[nx][ny] != 0) {
-						if (tempMap[nx][ny] > 1) {
-							q.offer(new int[] { nx, ny, tempMap[nx][ny] });
-						}
-						tempMap[nx][ny] = 0;
-					}
-				}
-			}
-		}
-	}
+    static int[][] copyMap(int[][] map) {
+        int[][] backup = new int[H][W];
+        for (int x = 0; x < H; x++) {
+            for (int y = 0; y < W; y++) {
+                backup[x][y] = map[x][y];
+            }
+        }
+        return backup;
+    }
 
-	// 밑으로 다운
-	static void gravity(int[][] tempMap) {
-		for (int c = 0; c < w; c++) {
-			Queue<Integer> q = new LinkedList<>();
+    static int findTopBrick(int y, int[][] map) {
+        int top = -1;
+        for (int x = 0; x < H; x++) {
+            if (map[x][y] > 0) {
+                top = x;
+                break;
+            }
+        }
+        return top;
+    }
 
-			for (int r = h - 1; r >= 0; r--) {
-				if (tempMap[r][c] != 0) {
-					q.offer(tempMap[r][c]);
-					tempMap[r][c] = 0;
-				}
-			}
+    static void boom(int x, int y, int[][] map) {
+        if (map[x][y] <= 0) {
+            return;
+        }
+        int range = map[x][y];
+        map[x][y] = 0;
+        for (int r = 1; r < range; r++) {
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir] * r;
+                int ny = y + dy[dir] * r;
+                if (nx >= 0 && ny >= 0 && nx < H && ny < W) {
+                    boom(nx, ny, map);
+                }
+            }
+        }
+    }
 
-			int r = h - 1;
-			while (!q.isEmpty()) {
-				tempMap[r][c] = q.poll();
-				r--;
-			}
-		}
-	}
-
-	static void countBricks(int[][] tempMap) {
-		int sum = 0;
-		for (int i = 0; i < h; i++) {
-			for (int k = 0; k < w; k++) {
-				if (tempMap[i][k] != 0) {
-					sum++;
-				}
-			}
-		}
-		MINBRICKS = Math.min(sum, MINBRICKS);
-	}
+    static void drop(int[][] map) {
+        for (int y = 0; y < W; y++) {
+            Queue<Integer> q = new LinkedList<>();
+            for (int x = H - 1; x >= 0; x--) {
+                if (map[x][y] != 0) {
+                    q.add(map[x][y]);
+                    map[x][y] = 0;
+                }
+            }
+            int index = H - 1;
+            while (!q.isEmpty()) {
+                map[index][y] = q.poll();
+                index--;
+            }
+        }
+    }
 }
